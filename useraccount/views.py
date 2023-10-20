b9e1dc6b4c1ef97d4def572b3bc7f7080ac0f2fc
linye <ericisnotrealname@protonmail.com>
2023-10-20 18:54:16 +0800

    userlogin

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView as DjangoLoginView
-# from django.contrib.auth.forms import 
-# from wagtail.views import LoginView as WagtailLoginView
 
 
 class DLoginView(DjangoLoginView):
     template_name = "login.html"
-    # form_class = 
-    pass
+    next_page = "/"
 
-
-# class WLoginView(WagtailLoginView):
-#     template_name = "login.html"
-#     ...
 
 # Create your views here.
